77fb5fe15a757dc801414af51b37cd463c75db57
devJeehee <148773471+jhee22@users.noreply.github.com>
2026-09-24 12:47:35 +0900

    [Passed] 한 가지로 열리는 자물쇠 (3ms, 0MB)

---
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int N;
+int a, b, c;
+
+int main() {
+    cin >> N;
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    int result = 0;
+    for (int i = 1; i <= N; i++) {    
+        for (int j = 1; j <= N; j++) {
+            for (int k = 1; k <= N; k++) {
+                if (abs(i - a) <= 2 ||
+                    abs(j - b) <= 2 ||
+                    abs(k - c) <= 2 
+                ) result++;
+            }
+        }
+    }
+
+    cout<< result; 
+
+    return 0;
+}
