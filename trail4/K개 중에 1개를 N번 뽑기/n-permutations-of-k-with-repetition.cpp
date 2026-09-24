b22efb600a7549979daba0bc7cb38f11483176ef
devJeehee <148773471+jhee22@users.noreply.github.com>
2026-09-24 14:04:21 +0900

    [Passed] K개 중에 1개를 N번 뽑기 (52ms, 1MB)

---
+++ trail4/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+int K, N;
+
+// 1. 지금까지 내가 선택한 값을 저장하는 vector 
+vector<int> selected; 
+
+// 2. depth번째 자리에 무엇을 넣을지 결정 
+void dfs(int depth) {
+    // 3. 종료 조건 : N 자리 모두 완성  
+    if (depth == N) {
+        // 5. 결과 출력 
+        for (int num : selected) {
+            cout << num << " "; 
+        }
+        cout << "\n"; 
+        return; 
+    }
+
+    // 4. 현재 선택 가능한 것들 
+    for (int i = 1; i <= K; i++) {
+        // 선택한 수 담기 
+        selected.push_back(i);
+
+        // 다음 자리 숫자 호출
+        dfs(depth+1); 
+
+        // *** 선택 취소 *** 
+        selected.pop_back(); 
+
+    }
+}
+
+int main() {
+    cin >> K >> N;
+
+    // Please write your code here.
+    dfs(0); 
+    
+    return 0;
+}
